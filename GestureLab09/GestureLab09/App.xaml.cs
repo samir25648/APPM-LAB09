93d1f6a435ffcd99b4097e41a92357de94ff8feb
samir25648 <samir.alanya@tecsup.edu.pe>
2023-06-19 23:39:04 -0500

    Agregar archivos de proyecto.

---
+++ GestureLab09/GestureLab09/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GestureLab09
+{
+    public partial class App : Application
+    {
+        public static float ScreenHeight { get; set; }
+
+        public static double ScreenWidth { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GestureLab09/GestureLab09/TapDemo.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GestureLab09
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TapDemo : ContentPage
+    {
+        int tapCount;
+        readonly Label label;
+        public TapDemo()
+        {
+            InitializeComponent();
+            var image = new Image
+            {
+                Source = "tapped.jpg",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.NumberOfTapsRequired = 2;
+            tapGestureRecognizer.Tapped += OnTapGestureRecognizerTapped;
+            image.GestureRecognizers.Add(tapGestureRecognizer);
+
+            label = new Label
+            {
+                Text = "tap the photo!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            Content = new StackLayout
+            {
+                Padding = new Thickness(20, 100),
+
+                Children =
+                {
+                    image,
+                    label,
+                }
+            };
+
+            void OnTapGestureRecognizerTapped(object sender, EventArgs arg)
+            {
+                tapCount++;
+                label.Text = String.Format("{0} tap{1} so far!",
+                    tapCount,
+                    tapCount == 1 ? "" : "s");
+
+                var imageSender = (Image)sender;
+
+                // watch the monkey go from color black&white!
+                if (tapCount % 2 == 0)
+                {
+                    imageSender.Source = "tapped.jgp";
+                }
+                else
+                {
+                    imageSender.Source = "tapped_bw.jpg";
+                }
+            }
+        }
+    }
+}
